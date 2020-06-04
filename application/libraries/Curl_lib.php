d156bc35940ca305850b90822c630eb50f972129
hilmawanyr <hilmawanyr@gmail.com>
2020-06-04 14:25:00 +0700

    refactor auth

--- application/libraries/Curl_lib.php
+++ application/libraries/Curl_lib.php
@@ -10,17 +10,17 @@ class Curl_lib
         $this->ci =& get_instance();
 	}
 
-	public function exec_curl($endpoint, $header, $post_req='', $body='')
+	public function exec_curl($endpoint, $header, $is_post_req='', $body='')
 	{
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $endpoint);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
 
-		if (!empty($post_req)) {
+		if (!empty($is_post_req)) {
 			curl_setopt($ch, CURLOPT_POST, 1);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
 		}
-		
+
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		$response = curl_exec($ch);
 		curl_close($ch);
--- application/modules/auth/controllers/Auth.php
+++ application/modules/auth/controllers/Auth.php
@@ -8,15 +8,20 @@ class Auth extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->library('auth_lib');
-		$this->http_header = $this->auth_lib->http_header;
-		$this->hostapi = $this->auth_lib->hostapi;
+		$this->http_header = ['x-bkd-key: '.APP_KEY, 'Content-Type: application/json'];
+		$this->hostapi = ENVIRONMENT == 'development' ? DEV_HOST_ENDPOINT : PRO_HOST_ENDPOINT;
+		$this->load->helper('cookie');
 	}
 
+	/**
+	 * Will be redirect to home if bkd_session or sia_cookie exist
+	 * 
+	 * @return void
+	 */
 	public function index()
 	{
 		if (!$this->session->userdata('bkd_session')) {
-			$is_cookie_exist = $this->auth_lib->is_has_cookie();
+			$is_cookie_exist = $this->_is_has_cookie();
 			if ($is_cookie_exist === FALSE) {
 				$data['page_title'] = 'Authentication';
 				$this->load->view('auth_v', $data);
@@ -26,6 +31,11 @@ class Auth extends CI_Controller {
 		}
 	}
 
+	/**
+	 * Handle login from manually login
+	 * 
+	 * @return void
+	 */
 	public function attemp_login()
 	{
 		$username = $this->input->post('username');
@@ -34,15 +44,72 @@ class Auth extends CI_Controller {
 		(empty($username) OR empty($password)) ? redirect('auth','refresh') : '';
 
 		$body = json_encode(['username' => $username, 'password' => $password]);
-		$exec = $this->auth_lib->exec_curl($this->hostapi.'/api_bkd/attemp_login', $this->http_header, 1, $body);
-		$this->auth_lib->decode_response($exec);
+		$exec = $this->curl_lib->exec_curl($this->hostapi.'/api_bkd/attemp_login', $this->http_header, 1, $body);
+		$this->_decode_auth_response($exec);
 	}
 
+	/**
+	 * Cookie from SIA login will be delete when user sign out
+	 * 
+	 * @return void
+	 */
 	public function logout()
 	{
 		$this->session->sess_destroy();
 		setcookie('sia_cookie','',time() - 7200,'/');
 		redirect('auth','refresh');
+	}
+
+	/**
+	 * Checking available cookie
+	 * 
+	 * @return void
+	 */
+	private function _is_has_cookie()
+	{
+		$cookie = get_cookie('sia_cookie');
+		if (isset($cookie)) {
+			$credential = explode('.', base64_decode($cookie));
+			$username   = $credential[0];
+			$usertype	= $credential[3];
+			$usergroup	= $credential[2];
+			$body       = json_encode(['username' => $username, 'usergroup' => $usergroup, 'usertype' => $usertype]);
+			$req = $this->curl_lib->exec_curl($this->hostapi.'/api_bkd/cookie_login', $this->http_header, 1, $body);
+			$this->_decode_auth_response($req);
+		}
+		return FALSE;
+	}
+
+	/**
+	 * Decode response to create session and give direct response for invalid login or permission
+	 * 
+	 * @return void
+	 */
+	private function _decode_auth_response($res)
+	{
+		$response = json_decode($res);
+
+		if ($response->status == 1) {
+			$this->_create_session((array)$response->data);
+		} elseif ($response->status == 3) {
+			$this->session->set_flashdata('fail', 'Login fail! Invalid username or password');
+			redirect('auth','refresh');
+		} else {
+			$this->session->set_flashdata('fail', 'Login fail! You don\'t have permission to access this application.');
+			delete_cookie('sia_cookie');
+			redirect('auth','refresh');
+		}
+	}
+
+	/**
+	 * Create main session
+	 * 
+	 * @return void
+	 */
+	protected function _create_session($data)
+	{
+		$this->session->set_userdata('bkd_session', $data);
+		redirect('/','refresh');
 	}
 
 }
